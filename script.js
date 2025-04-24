3d20a8d3a9fe6596249362d663f80c3c01364771
visH424 <vishalrathor355@gmail.com>
2025-04-24 11:48:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*const img= document.getElementById('user-img');
-const name=document.getElementById('user-name');
-const imail=document.getElementById('user-imail');
-const location=document.getElementById('user-location');
-const btn=document.getElementById('generate-btn');
-
-async function getrandomuser() {
-  try{
-    const response=await fetch("https://randomuser.me/api/");
-    const data=await response.json();
-    const user=data.results[0];
-
-    img.src = user.picture.large;
-    name.textContent = `${user.name.first} ${user.name.last}`;
-    imail.textContent = `📧 ${user.email}`;
-    location.textContent = `📍 ${user.location.city}, ${user.location.country}`;
-  } catch (error) {
-    console.error("Error fetching user:", error);
-  }
-}
-btn.addEventListener('click',getrandomuser);
-getrandomuser();*/
 
 const img = document.getElementById("user-img");
 const nameEl = document.getElementById("user-name");
